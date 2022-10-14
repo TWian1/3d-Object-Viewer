06866d0e06597db31e6fbf0a7fe52af4bf730d1e
Ian <80492876+TheWolfIan1@users.noreply.github.com>
2022-10-13 23:41:51 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,263 @@
+from math import floor, tan, sin, cos, sqrt
+import time
+
+from pyparsing import col
+def main():
+  Cubetris = openobj("teapot")
+  width = 420
+  height = 420
+  background = background_maker(width, height)
+  matProj = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]]
+  fNear = 1.0
+  fFar = 1000.0
+  fFov = 90.0
+  fAspectRatio = height/width
+  fFovRad = 1.0/tan(fFov*0.5)
+  matProj[0][0] = fAspectRatio*fFovRad
+  matProj[1][1] = fFovRad
+  matProj[2][2] = fFar / (fFar-fNear)
+  matProj[3][2] = (-fFar*fNear) / (fFar-fNear)
+  matProj[2][3] = 1.0
+  matProj[3][3] = 0.0
+  vCamera = [0, 0, 0]
+  angle = 0
+  for f in range(1):
+    time1 = time.time()
+    alldraw = []
+    angle += 1/5 
+    background = background_maker(width, height)
+    rotation_x = [
+            [1, 0, 0, 0],
+            [0, cos(angle*0.5), sin(angle*0.5), 0],
+            [0, -sin(angle*0.5), cos(angle*0.5), 0],
+            [0,0,0,1]
+        ]
+    rotation_z = [
+              [cos(angle), sin(angle), 0, 0],
+              [-sin(angle), cos(angle), 0, 0],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1]
+          ]
+    k = 100
+    q = 0
+    for tri in Cubetris:
+      q+= 1
+      while k >255:
+        k -=100
+      k += 57
+      triprojected = [[0,0,0], [0,0,0], [0,0,0]]
+      trirx = [[0,0,0], [0,0,0], [0,0,0]]
+      trirz = [[0,0,0], [0,0,0], [0,0,0]]
+      for a in range(3): Multiplymatrix(tri[a], trirz[a], rotation_z)
+      for a in range(3): Multiplymatrix(trirz[a], trirx[a], rotation_x)
+      tritranslated = trirx
+      for a in range(3): tritranslated[a][2] = trirx[a][2] + 3
+      line1 = [0, 0, 0]
+      line2 = [0, 0, 0]
+      normal = [0, 0, 0]
+      for a in range(3): line1[a] = tritranslated[1][a]-tritranslated[0][a]
+      for a in range(3): line2[a] = tritranslated[2][a]-tritranslated[0][a]
+      for a in range(3): normal[a] = line1[(a+1)%3]*line2[(a+2)%3] - line1[(a+2)%3]*line2[(a+1)%3]
+      l = sqrt(normal[0]*normal[0] + normal[1]*normal[1] + normal[2]*normal[2])
+      normal = [t/l for t in normal]
+      if normal[2] < 0:
+        light_direction = [0, 0, -1]
+        l = sqrt(light_direction[0]*light_direction[0] + light_direction[1]*light_direction[1] + light_direction[2]*light_direction[2])
+        light_direction = [t/l for t in light_direction]
+        dp = round((normal[0]*light_direction[0] + normal[1]*light_direction[1] + normal[2]*light_direction[2])*230)+25
+        for a in range(3):Multiplymatrix(tritranslated[a], triprojected[a], matProj)
+        for a in range(6):triprojected[floor(a/2)][a%2] = (triprojected[floor(a/2)][a%2]+1.0)*(0.5*width)
+        triprojected.append([dp, dp, dp, 255])
+        alldraw.append(triprojected)
+    for triproj in alldraw: drawtri(background, round(triproj[0][0]), round(triproj[0][1]), round(triproj[1][0]), round(triproj[1][1]), round(triproj[2][0]), round(triproj[2][1]), fill=True, fillcolor=triproj[3], color=triproj[3])
+    for a in imgPrint(background, pre=True): print(a)
+    print(time.time()-time1)
+    print(f'\033[{211}A', end='\x1b[2K')
+    
+def drawtris(list, set, color=[255,255,255,255], gen=False, bgcolor=[0,0,0,0]):
+  if gen:
+    list = []
+    maxy, maxx, miny, minx = max([max([x[1] for x in y]) for y in set]), max([max([x[0] for x in y]) for y in set]), min([min([x[1] for x in y]) for y in set]), min([min([x[0] for x in y]) for y in set])
+    for a in range(maxy - miny * (maxx - minx)):
+      if a % (maxx - minx) == 0: list.append([])
+      list[floor(a/(maxx - minx))].append(bgcolor)
+    for a in set: drawtri(list, a[0][0]-minx, a[0][1]-miny, a[1][0]-minx, a[1][1]-miny, a[2][0]-minx, a[2][1]-miny) 
+    return list
+  else: 
+    for a in set: drawtri(list, a[0][0], a[0][1], a[1][0], a[1][1], a[2][0], a[2][1])
+def Multiplymatrix(i, o, m):
+  o[0] = i[0] * m[0][0] + i[1] * m[1][0] + i[2] * m[2][0] + m[3][0]
+  o[1] = i[0] * m[0][1] + i[1] * m[1][1] + i[2] * m[2][1] + m[3][1]
+  o[2] = i[0] * m[0][2] + i[1] * m[1][2] + i[2] * m[2][2] + m[3][2]
+  w = i[0] * m[0][3] + i[1] * m[1][3] + i[2] * m[2][3] + m[3][3]
+  if w != 0: o = [x/w for x in o]
+def background_maker(x, y='default', color=[0,0,0,0]):
+  out = []
+  if y == 'default': y = x
+  for a in range(y*x):
+    if a%x==0:out.append([])
+    out[floor(a/x)].append(color)
+  return out
+def openobj(filename):
+  file = open(filename + ".txt").readlines()
+  verts = []
+  faces = []
+  for a in file:
+    if a[0] == 'v': verts.append([float(x) for x in a[2:][:-1].split(' ')])
+    if a[0] == 'f': faces.append([int(x) for x in a[2:][:-1].split(' ')])
+  out = []
+  maximum = 0
+  for a in verts:
+    if max([abs(b) for b in a]) > maximum: maximum = max([abs(b) for b in a])
+  for b in faces:
+    out.append([])
+    for c in b:
+      out[len(out)-1].append([q/maximum for q in verts[c-1]])
+  return out
+def setpix(list,x,y,color=[255,255,255,255]):
+  try:list[y][x] = color 
+  except:pass
+def setline(list,x,y,x2,y2,color=[255,255,255,255],p='default',gen=False, alr = False, lim = 2000): 
+  if gen: 
+    if x < x2: x,x2 = 0, x2-x
+    else: x2,x = 0, x-x2
+    if y < y2: y,y2 = 0, y2-y
+    else: y2,y = 0, y-y2
+    list = []
+    for a in range(round(abs(y2-y)+1)*round(abs(x2-x)+1)):
+      if a%round(abs(x2-x))==0:list.append([])
+      list[floor(a/round(abs(x2-x)))].append([0,0,0,0])
+  if (x == x2 and y == y2) or (abs(x-x2) <= 1 and abs(y-y2) <=1 ):
+    list[y][x] = color
+    if gen:return list
+    else: return 0
+  if x2-x == 0:
+    if y2 > y: vert = 1
+    else: vert = -1
+    yinter = y
+  else:
+    vert =0
+    s=(y2-y)/(x2-x)
+    yinter = (x*y2 - x2*y)/(x-x2)
+  s2 = 1
+  if x2-x < 0: s2 = -1
+  xpos,ypos, = x,y
+  if p == 'default': p = 1000
+  toprint = []
+  while(xpos < len(list[0])+lim and xpos > 0-lim and ypos < len(list)+lim and ypos > 0-lim):
+    xpos += (1/p)*s2
+    if vert == 0:ypos = (xpos*s)+yinter
+    else: ypos += vert
+    
+    if (round(xpos) == x2 and round(ypos) == y2) or (abs(xpos-x2) <= 1 and abs(ypos-y2) <=1):
+      toprint.append([round(xpos), round(ypos)])
+      break
+    toprint.append([round(xpos), round(ypos)])
+  for a in toprint: setpix(list, a[0], a[1],color)
+  if gen: return list
+
+def upscale(pixelsarray, scale, limx, limy):
+  out = []
+  for a in range(len(pixelsarray)*scale):
+    out.append([])
+    for b in range(len(pixelsarray[0])*scale):out[a].append([])
+  for a in range(len(pixelsarray)):
+    for b in range(len(pixelsarray[0])):
+      for c in range(scale):
+        for d in range(scale): out[(a*scale)+c][(b*scale)+d] = pixelsarray[a][b]
+  return out
+def drawtri(list, x1, y1, x2, y2, x3, y3, color=[255, 255, 255, 255], gen=False, bgcolor = [0, 0, 0, 0], fill = False, fillcolor = [255, 255, 255]):
+  dat = [x1, y1, x2, y2, x3,y3]
+  if gen:
+    list = []
+    for a in range((max([y1, y2, y3]) - min([y1, y2, y3])) * (max([x1, x2, x3]) - min([x1, x2, x3]))):
+      if a % (max([x1, x2, x3]) - min([x1, x2, x3])) == 0: list.append([])
+      list[floor(a/(max([x1, x2, x3]) - min([x1, x2, x3])))].append(bgcolor)
+  if fill:
+    filltri(list, x1, y1, x2, y2, x3, y3, fillcolor)
+  for a in range(3): setline(list, dat[a*2], dat[(a*2)+1], dat[((a*2)+2) % 6], dat[(((a*2)+2) % 6)+1], color=color)
+  if gen: return list
+def filltri(list, x1, y1, x2, y2, x3, y3, color):
+  A = area(x1, y1, x2, y2, x3, y3)
+  minx, miny, maxx, maxy = min(x1, x2, x3), min(y1, y2, y3), max(x1, x2, x3), max(y1, y2, y3)
+  for y in range(len(list)):
+    if y > maxy:break
+    if y < miny:continue
+    for x in range(len(list[0])):
+      if x > maxx:break
+      if x < minx:continue
+      A1 = area(x, y, x2, y2, x3, y3)
+      A2 = area(x1, y1, x, y, x3, y3)
+      A3 = area(x1, y1, x2, y2, x, y)
+      if A == A1 + A2 + A3: list[y][x] = color
+def area(x1, y1, x2, y2, x3, y3):
+  return abs((x1*(y2-y3) + x2*(y3-y1)+ x3*(y1-y2))/2.0)
+def acol(list, col=[0, 0, 0]): return combine(background_maker(len(list[0]), len(list)), list, 0, 0, 255)
+def imgPrint(pixelsarray,pre=False):
+  height,width,total = len(pixelsarray),len(pixelsarray[0]),[]
+  for a in range(floor(height/2)):
+    string_temp = ""
+    for b in range(width):
+      clr = [0,0,0,0,0,0]
+      try:
+        for c in range(3): clr[c] = pixelsarray[floor(a*2)+1][floor(b)][c]
+        for c in range(3): clr[c+3] = pixelsarray[floor(a*2)][floor(b)][c]
+      except Exception: pass
+      string_temp += f"\033[38;2;{clr[0]};{clr[1]};{clr[2]}m\033[48;2;{clr[3]};{clr[4]};{clr[5]}m▄\033[0m"
+    if pre == False: print(string_temp)
+    else: total.append(string_temp)
+  if pre: return total
+def combine(original, image, x, y, alpha=255):
+  orig = original
+  for a in range(len(original)):
+    for b in range(len(original[0])):
+      if a >= y and b >= x and b <= x+len(image[0]) and a<=y+len(image):
+        try:
+          alpha2 = (image[a-y][b-x][3]/255)*(alpha/255)
+          orig[a][b] = [round((orig[a][b][z]*(1-alpha2))+(image[a-y][b-x][z] * alpha2)) for z in range(3)]
+        except Exception: pass
+  return orig
+def videoPrint(arrays, slow=1):
+  for x in arrays:
+    print(len(x))
+    upcurs = int(len(x)+3)
+    for a in range(slow):
+      for b in x: print(b)
+      print(f'\033[{upcurs}A', end='\x1b[2K')
+def textgen(text, text_color=[255, 255, 255], text_opacity=255, background_opacity=0,  background_color=[0,0,0], return_size=False):
+  valtext = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789,./\\?!()+-_:;'\""
+  data = ['100000110000011000001011111001000100100010001010000101000001000', '011011001111001001111000101110', '111111010000011000001100000110000101111100100001010000101111100', '111101000110001111101000010000', '001111001000011000000100000010000001000000100000001000010011110', '011101000110000100001000101110', '111110010000101000001100000110000011000001100000110000101111100', '011111000110001011110000100001', '111111110000001000000100000011111111000000100000010000001111111', '011101000111100100101000101110', '100000010000001000000100000011111111000000100000010000001111111', '010001000100111001000011', '001110001000101000001100000110011101000000100000001000000011100', '011100000101111100011001101101', '100000110000011000001100000111111111000001100000110000011000001', '100011000111001101101000010000', '111111100010000001000000100000010000001000000100000010001111111', '111101', '011111010000010000001000000100000010000001000000100000010000001', '011101000100001000010000000001', '100000110000101000100100100011100001001000100010010000101000001', '100110101100101010011000', '111111110000001000000100000010000001000000100000010000001000000', '011010101010', '100000110010011001001101010110101011010101110001111000111000001', '1010110101101011101010000', '100000110000111000101100010110010011010001101000111000011000001', '1000110001100011100110110', '011111010000011000001100000110000011000001100000110000010111110', '0111010001100011000101110', '100000010000001000000111111010000011000001100000110000010111110', '100010001110100110010110', '001110101000101000101100000110000011000001100000101000100011100', '000110001001110100101001001100', '100001100010100100101000111110100001100001100001011110', '1000010000100001100010111', '111111000000010000001000000101111101000000100000010000000111111', '11100001011010000111', '000100000010000001000000100000010000001000000100000010001111111', '001010010010111010', '011111010000011000001100000110000011000001100000110000011000001', '0111110001100011000110001', '000100000101000010100010001001000100100010100000110000011000001', '0010001010100011000110001', '010001010101011010101101010110101011001001100000110000011000001', '0101010101101011000110001', '100000110000010100010001010000010000010100010001010000011000001', '1000101010001000101010001', '000100000010000001000001010001000100100010100000110000011000001', '011101000100001011111000110001', '111111110000000100000001000000010000000100000001000000011111111', '111111000001000001000001011111', '011111010000111000101100010110010011010001101000111000010111110', '111111100010000001000000100000010001001000010100000110000001000', '011111110000001000000100000001111100000001000000100000011111110', '111111000000010000001000000100111100000001000000100000011111110', '000010000001000000100000010001111111000100100010010001001000100', '011111010000010000001000000101111101000000100000010000001111111', '011111010000011000001100000111111101000000100000001000000011110', '100000001000000010000000100000001000000010000000100000011111111', '011111010000011000001100000101111101000001100000110000010111110', '011100000001000000010000000101111111000001100000110000010111110', '11', '1', '100001000001000010000010000010000100000100001', '000010000100010000100010001000010001000010000', '001000000000100001000001000001000010000111110', '101111111', '001010010100100100010010001', '100010010001001001010010100', '000000000000010000100111100010000100000000000', '000000000000000000000111100000000000000000000', '011111100000000000000000000000000000000000000000000000000000000', '000000000', '000100000000000000', '000000000', '000000000000000000010010000', '011111101000000100100010000001001000100010010111001000000111111']
+  datas = [[7,9,0], [5,6,0], [7,9,0], [5,6,0], [7,9,0], [5,6,0], [7,9,0], [5,6,0], [7,9,0], [5,6,0], [7,9,0], [4,6,0], [7,9,0], [5,6,-1], [7,9,0], [5,6,0], [7,9,0], [1,6,0], [7,9,0], [5,6,-1], [7,9,0], [4,6,0], [7,9,0], [2,6,0], [7,9,0], [5,5,0], [7,9,0], [5,5,0], [7,9,0], [5,5,0], [7,9,0], [4,6,-1], [7,9,0], [5,6,-1], [6,9,0], [5,5,0], [7,9,0], [4,5,0], [7,9,0], [3,6,0], [7,9,0], [5,5,0], [7,9,0], [5,5,0], [7,9,0], [5,5,0], [7,9,0], [5,5,0], [7,9,0], [5,6,-1], [7,9,0], [5,6,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [7,9,0], [1,2,0], [1,1,0], [5,9,0], [5,9,0], [5,9,0], [1,9,0], [3,9,0], [3,9,0], [5,9,0], [5,9,0], [7,9,0], [1,9,0], [2,9,0], [1,9,0], [3,9,0], [7,9,0]]
+  #          A         a        B        b        C        c        D        d        E        e        F        f        G         g       H         h        I        i        J        j         K        k        L        l        M        m        N        n        O        o        P         p        Q         q        R        r        S        s        T        t        U        u        V        v        W        w        X         x       Y        y         Z         z       0        1        2        3        4        5        6        7        8        9        ,         .       /        \        ?         !       (         )       +        -        _        :         ;        '        "     missing
+  total_length, total_height,indexes,out,curlength = 1,0,[],[],1
+  for a in text:
+    if a == " ":
+      indexes.append(-1)
+      total_length += 4
+    else:
+      try: index = valtext.index(a)
+      except: index = 77
+      if datas[index][1] > total_height-2: total_height = datas[index][1]+2
+      total_length += ((datas[index][0]) + 1)
+      indexes.append(index)
+  for a in range(total_height):
+    out.append([])
+    for b in range(total_length): out[a].append([background_color[0], background_color[1], background_color[2], background_opacity])
+  for a in indexes:
+    if a == -1:
+      curlength += 4
+      continue
+    for b in range(datas[a][1]):
+      for c in range(datas[a][0]):
+        x = (b*datas[a][0]) + c
+        if data[a][x] == "1": out[(total_height-b)-2][c+curlength] = [text_color[0], text_color[1], text_color[2], text_opacity]
+    curlength += datas[a][0]+1
+  if return_size: return out, total_length, total_height
+  return out
+
+if __name__ == "__main__":
+  main()
